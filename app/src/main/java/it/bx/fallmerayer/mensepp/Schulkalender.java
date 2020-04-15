61139cddba4ca4ca312fca2f2217aec66996a19c
Jonas Lechner <jonaslechnner23456789@gmail.com>
2020-04-15 18:58:58 +0200

    Mainklasse zum testen; dateiToKalender ausprogrammitert und txt-Datei mit Kalender erstellt

--- app/src/main/java/it/bx/fallmerayer/mensepp/Schulkalender.java
+++ app/src/main/java/it/bx/fallmerayer/mensepp/Schulkalender.java
@@ -1,9 +1,41 @@
 package it.bx.fallmerayer.mensepp;
 
-public class Schulkalender {
-    private int [][] kalender=new int[12][31];
+import android.os.Build;
 
+import androidx.annotation.RequiresApi;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Schulkalender {
+    private int [][] kalender=new int[31][12];
+    private String  dateipfad="kalender.txt";
     public Schulkalender() {
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void dateiToKalender(){
+        int monate=0;
+        int tage=0;
+        String [] values;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(dateipfad))) {
+            String line;
+            while (monate<12) {//dass jede Zeile durchgegangen wird
+                line = br.readLine();
+                values=line.split("  ");
+                for (tage=0;tage<31;++tage){//int Integer umwandeln
+                    kalender[tage][monate]=Integer.parseInt(values[tage]);
+                }
+                ++monate;
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
