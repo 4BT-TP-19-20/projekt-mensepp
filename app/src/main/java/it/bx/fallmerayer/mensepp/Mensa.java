76a737a0d2562d10a68d16237b631a6eee10ef1f
Beniamin Terza <beniaminterza@gmail.com>
2020-04-15 17:19:50 +0200

    methode für mensa klasse

--- app/src/main/java/it/bx/fallmerayer/mensepp/Mensa.java
+++ app/src/main/java/it/bx/fallmerayer/mensepp/Mensa.java
@@ -1,4 +1,36 @@
 package it.bx.fallmerayer.mensepp;
 
 public class Mensa {
+    private String[][] mensaplan = new String[7][5];
+    private Schulkalender schulkalender = new Schulkalender(".csv");
+    private String[][] schulkalender = new String[7][5];
+    private String[][] schulkalederMitEssen = new String[12][31];
+
+    public void schulkalenderMitEssenFuellen() {
+        int wochenPointer = 0;
+        int lastSchultag = 0;
+        int ferienTage = 0;
+        for (int monat = 8; monat < 12; monat++) {
+            for (int tag = 0; tag < 31; tag++) {
+                if (schulkalender[monat][tag] > 0) {
+                    if (wochenPointer == 7) {
+                        wochenPointer = 0;
+                    }
+                    if (schulkalender[monat][tag] <= lastSchultag) {
+                        wochenPointer += 1;
+                    } else if (ferienTage >= 7) {
+                        wochenPointer += 1;
+                    }
+                    schulkalederMitEssen[monat][tag] = mensaplan[wochenPointer][tag];
+                    lastSchultag = schulkalender[monat][tag];
+                    ferienTage = 0;
+                } else if (schulkalender[monat][tag] == 0) {
+                    ferienTage += ferienTage;
+                }
+                if (monat == 11 && tag == 30) {
+                    monat = -1;
+                }
+            }
+        }
+    }
 }
